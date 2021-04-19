108e0fe24dfb6f84f7a35591187c9a1b88399a90
gabriel-piedade95 <45984859+gabriel-piedade95@users.noreply.github.com>
2021-04-19 16:30:30 -0300

    correcao e simplificacao do codigo

--- Inferencia_Redes.py
+++ Inferencia_Redes.py
@@ -1,132 +1,63 @@
-import sympy as sym
 import operator as op
 import itertools as iter
-import random as rand
+import numpy as np
 
-## estados & matriz ##
-
-def transforma_estados(estados):
-
-	for i in range(0, len(estados)):
-
-		estados[i] = sym.Matrix(estados[i])
-
-	return estados
-
-def cria_matriz(tam):
-
-  mtx = []
-  for i in range(0, tam):
-    aux = []
-    for j in range(0, tam):
-      aux.append(sym.Symbol(f'A{i}{j}'))
-
-    mtx.append(aux)
-
-  return sym.Matrix(mtx)
-
-## inequacoes ##
-
-def gera_inequacoes(estados):
-
-  resultados = []
-  inequacoes = []
-
-  for i in range(0, len(estados) - 1):
-    res = cria_matriz(len(estados[i]))*estados[i]
-    resultados.append(res) 
-
-
-  for i in range(0, len(resultados)):
-    for j in range(0, len(resultados[i])):
-      inequacoes.append(seleciona_inequacao(resultados[i][j], estados[i][j], estados[i + 1][j]))
-
-  return inequacoes
-
-def gera_restricoes(estados):
-
-  ineqs = gera_inequacoes(estados)
-  restricoes = {}
-
-  for i in range(0, len(estados[0])):
-    aux = []
-    for j in range(0, len(estados) - 1):
-      aux.append(ineqs[(len(estados[i]) * j) + i])
-
-    restricoes[i] = aux
-
-  return restricoes
-
-
-def seleciona_inequacao(ineq, est1, est2):
+def seleciona_inequacao(est1, est2):
 
   if est1 == 1:
     if est2 == 0:
-      return op.lt(ineq, 0)
+      return op.lt
     elif est2 == 1:
-      return  op.ge(ineq, 0)
+      return  op.ge
 
   elif est1 == 0:
     if est2 == 0:
-      return op.le(ineq, 0)
+      return op.le
     elif est2 == 1:
-      return  op.gt(ineq, 0)
+      return  op.gt
 
-
-## estados ##
-
-def gera_estados(dom):
-
-  aux = [dom[x] for x in dom]
-
-  for possibilidade in list(iter.product(*aux)):
+def inequacoes(estados):
+  
+  ineqs = {}
+  for i in range(0, 11):
     
-    yield possibilidade
-
-## Resolve CSP ##
-
-def testa_resticao(rest, dom, estados):
-
-  ineqs = list.copy(rest)
-  for i in range(0, len(ineqs)):
-    x = 0
-    for var in dom:
-      ineqs[i] = ineqs[i].subs(var, estados[x])
-      x += 1
-    if ineqs[i] == False:
-      break
+    aux = []
+    for j in range(0, len(estados) -1):
+      aux.append(seleciona_inequacao(estados[j][i], estados[j + 1][i]))
+  
+    ineqs[i] = aux
 
   return ineqs
 
-def resolve_CSP(rest, dom):
-
-  resultados = []
-  estados = gera_estados(dom)
-  while True:
-    try:
-      estado = next(estados)
-      teste = testa_resticao(rest, dom, estado)
-      if all(teste):
-        resultados.append(estado)
-      
-    except StopIteration:
-      break
+def resolve_CSP(estados):
+  
+  linhas_possiveis = list(iter.product((-1, 0, 1), repeat=11))
+  ineqs = inequacoes(estados)
+  resultados = {}
+  for i in range(0, len(ineqs)):
+    restricoes = ineqs[i]
+    resultados[i] = []
+    for linha in linhas_possiveis:
+      aux = [False] * len(restricoes)
+      for r in range(0, len(restricoes)):
+        prod = np.array(linha) * np.array(estados[r])  
+        if restricoes[r](sum(prod), 0):
+          aux[r] = True
+        else:
+          break
+      if all(aux):
+        resultados[i].append(linha)
+  
   return resultados
 
-def gera_linhas_possiveis(estados):
 
-  estados =  transforma_estados(estados)
-  mat = cria_matriz(len(estados[0]))
-  restricoes = gera_restricoes(estados)
-  for i in range(0, mat.shape[0]):
-    var = mat.row(i)
-    rest = restricoes[i]
-    dom = {} 
-    for k in var:
-      dom[k] = (-1, 0, 1)
+def imprime_solucoes(solucoes):
 
-    with open(f'gene{i}.txt','w', encoding='utf-8') as f:
-      for solucao in resolve_CSP(rest, dom):
-        for est in solucao:
-          f.write(str(est) + '  ')
+  for gene in range(0, len(solucoes)):
+    s = solucoes[gene]
+    with open(f'gene{gene}.txt', 'w', encoding= 'utf-8') as f:
+      for linha in range(0, len(s)):
+        l = s[linha]
+        for j in range(0, len(l)):
+          f.write(str(l[j]) + ' ')
         f.write('\n')
