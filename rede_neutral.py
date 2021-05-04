504d17c3b1bc32bfcf4e8ad4a6ad3cb22bf203bb
gpsp95 <45984859+gabriel-piedade95@users.noreply.github.com>
2021-05-04 16:38:26 -0300

    limpeza do modulo

--- rede_neutral.py
+++ rede_neutral.py
@@ -8,29 +8,6 @@ mtz = dados.matriz_wildtype
 dic_poss = {-1: [-1], 0: [0], 1: [1], -2: [-1, 0], 2: [0, 1], 3: [-1, 1], 5: [-1, 0, 1]}
 l = [5, 5, 5, 5, 5]
 
-def gera_linha_possivel_colapsada(gene):
-
-	linhas = bios.abre_arquivo(f'gene{gene}.txt')
-	dominios = [[]] * 11
-
-	for i in range(0, len(linhas)):
-		
-		for j in range(0, len(dominios)):
-
-			if linhas[i][j] not in dominios[j]:
-
-				dominios[j].append(linhas[i][j])
-
-	for k  in range(0, len(dominios)):
-
-		for w in dic_poss:
-
-			if dic_poss[w] == sorted(dominios[k]):
-
-				dominios[k] = w 
-
-	return dominios
-
 
 def linhas_com_distancia_n(linhas_possiveis, distancia):
 
@@ -43,7 +20,3 @@ def linhas_com_distancia_n(linhas_possiveis, distancia):
 
 	return soma
 
-
-
-for i in range(0, 11):
-	print(gera_linha_possivel_colapsada(i))
