13437f600bfacd4a7ce62115e88cdeb9f8d6357e
gpsp95 <45984859+gabriel-piedade95@users.noreply.github.com>
2021-05-11 19:12:29 -0300

    correcao da funcao

--- rede_neutral.py
+++ rede_neutral.py
@@ -9,14 +9,18 @@ dic_poss = {-1: [-1], 0: [0], 1: [1], -2: [-1, 0], 2: [0, 1], 3: [-1, 1], 5: [-1
 l = [5, 5, 5, 5, 5]
 
 
-def linhas_com_distancia_n(linhas_possiveis, distancia):
+def distribuicao_distancia_Hamming(mtz, n):
 
-	soma = 0
-	for i in iter.combinations(linhas_possiveis, distancia):
-		prod = 1
-		for j in range(0, len(i)):
-			prod *= (len(dic_poss[i[j]]) -1)
-		soma += prod
+  dic_poss = {-1: [-1], 0: [0], 1: [1], -2: [-1, 0], 2: [0, 1], 3: [-1, 1], 5: [-1, 0, 1]}
+  tam = len(mtz)
+  mtz_linha = np.matrix(mtz).reshape(1, tam**2)
+  soma = 0
+  for combinacao in iter.combinations(mtz_linha.tolist()[0], n):
+    prod = 1
+    for i in range(0, len(combinacao)):
 
-	return soma
+      prod *= (len(dic_poss[combinacao[i]]) - 1)
+    
+    soma += prod
 
+  return soma
