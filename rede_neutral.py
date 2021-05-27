ae57787ec4ffe4257153f00f94efaacc53ff14da
gpsp95 <45984859+gabriel-piedade95@users.noreply.github.com>
2021-05-27 19:36:55 -0300

    limiar d calculado a partir de HxW

--- rede_neutral.py
+++ rede_neutral.py
@@ -6,10 +6,6 @@ import convergencia_redes as cR
 import math 
 
 
-global h_WT, w_WT, mtz_WT
-mtz_WT = dados.matriz_wildtype
-h_WT, w_WT = calculos_matriz(mtz_WT)
-
 
 def distanciaHamming(linha_matriz, linha_possivel):
 
@@ -33,15 +29,11 @@ def calculos_matriz(mtz):
 
 	return H, W
 
-def Q(limiar, H, W):
 
-	dif_H = abs(H - h_WT)
-	dif_W = abs(W - w_WT)
-	D = math.sqrt((dif_W**2) + (dif_H**2))
-	return D == limiar
+import math
+mtz_WT = dados.matriz_wildtype
+h_WT, w_WT = calculos_matriz(mtz_WT)
 
-
-mtz = dados.matriz_wildtype
 linhas_possiveis = [0] * 11
 
 for i in range(0, 11):
@@ -51,7 +43,8 @@ for i in range(0, 11):
 
 	for j in range(0, len(linhas_genes)):
 
-		if distanciaHamming(linhas_genes[j], mtz[i]):
+
+		if distanciaHamming(linhas_genes[j], mtz_WT[i]):
 			aux.append(linhas_genes[j])
 
 	linhas_possiveis[i] = aux
@@ -67,20 +60,17 @@ for i in range(0, 11):
 		nova_matriz = mtz_WT.copy()
 		nova_matriz[i] = linha
 		h, w = calculos_matriz(nova_matriz)
-		Hs.append(h)
-		Ws.append(w)
-
+		d = math.sqrt((abs(h - h_WT)**2) + (abs((w - w_WT)/2048)**2))
+		if d <= 0.1:
+			Hs.append(h)
+			Ws.append(w/2048)
 
 import matplotlib.pyplot as plt
 
 
-plt.scatter(Hs, Ws, color = 'green')
+plt.scatter(Hs, Ws)
 plt.title('Gráfico Entropia(H) por Convergência(W)')
 plt.xlabel('Entropia(H)')
 plt.ylabel('Convergência(W)')
-plt.scatter(h_WT, w_WT, color = 'red')
+plt.scatter(h_WT, w_WT/2048, color = 'red')
 plt.show()
-
-
-
-
