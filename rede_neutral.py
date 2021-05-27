77af9fecbfffdefb987ad5b2545ca8e1c6235e9f
gpsp95 <45984859+gabriel-piedade95@users.noreply.github.com>
2021-05-27 16:55:32 -0300

    novo metodo para gera o grafo

--- rede_neutral.py
+++ rede_neutral.py
@@ -1,26 +1,86 @@
 import itertools as iter 
 import dados
 import BioSist as bios 
+import histogramas as hist 
+import convergencia_redes as cR
+import math 
+
+
+global h_WT, w_WT, mtz_WT
+mtz_WT = dados.matriz_wildtype
+h_WT, w_WT = calculos_matriz(mtz_WT)
+
+
+def distanciaHamming(linha_matriz, linha_possivel):
+
+	a = 0
+	for i in range(0, len(linha_matriz)):
+
+		if linha_matriz[i] != linha_possivel[i]:
+			a += 1
+
+	return a == 1
+
+
+def calculos_matriz(mtz):
+
+	trns = bios.gera_transicoes(mtz)
+	lista_transicoes = bios.lista_adjacencia(trns)
+	bacias = bios.bacias(lista_transicoes)
+
+	H = bios.entropia(bacias)
+	W = cR.cal_W_total(lista_transicoes)
+
+	return H, W
+
+def Q(limiar, H, W):
+
+	dif_H = abs(H - h_WT)
+	dif_W = abs(W - w_WT)
+	D = math.sqrt((dif_W**2) + (dif_H**2))
+	return D == limiar
 
 
 mtz = dados.matriz_wildtype
+linhas_possiveis = [0] * 11
 
-dic_poss = {-1: [-1], 0: [0], 1: [1], -2: [-1, 0], 2: [0, 1], 3: [-1, 1], 5: [-1, 0, 1]}
-l = [5, 5, 5, 5, 5]
+for i in range(0, 11):
+	
+	linhas_genes = bios.abre_arquivo(f'gene{i}.txt')
+	aux = []
+
+	for j in range(0, len(linhas_genes)):
+
+		if distanciaHamming(linhas_genes[j], mtz[i]):
+			aux.append(linhas_genes[j])
+
+	linhas_possiveis[i] = aux
 
 
-def distribuicao_distancia_Hamming(mtz, n):
+Hs = []
+Ws = []
 
-  dic_poss = {-1: [-1], 0: [0], 1: [1], -2: [-1, 0], 2: [0, 1], 3: [-1, 1], 5: [-1, 0, 1]}
-  tam = len(mtz)
-  mtz_linha = np.matrix(mtz).reshape(1, tam**2)
-  soma = 0
-  for combinacao in iter.combinations(mtz_linha.tolist()[0], n):
-    prod = 1
-    for i in range(0, len(combinacao)):
+for i in range(0, 11):
 
-      prod *= (len(dic_poss[combinacao[i]]) - 1)
-    
-    soma += prod
+	for linha in linhas_possiveis[i]:
 
-  return soma
+		nova_matriz = mtz_WT.copy()
+		nova_matriz[i] = linha
+		h, w = calculos_matriz(nova_matriz)
+		Hs.append(h)
+		Ws.append(w)
+
+
+import matplotlib.pyplot as plt
+
+
+plt.scatter(Hs, Ws, color = 'green')
+plt.title('Gráfico Entropia(H) por Convergência(W)')
+plt.xlabel('Entropia(H)')
+plt.ylabel('Convergência(W)')
+plt.scatter(h_WT, w_WT, color = 'red')
+plt.show()
+
+
+
+
