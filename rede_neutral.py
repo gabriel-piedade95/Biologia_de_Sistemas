2a24b24cc86ebf4ad11d3e0ff6c84209f8fc5356
gpsp95 <45984859+gabriel-piedade95@users.noreply.github.com>
2021-05-28 00:54:00 -0300

    inicio do algoritmo genetico

--- rede_neutral.py
+++ rede_neutral.py
@@ -1,10 +1,8 @@
-import itertools as iter 
 import dados
 import BioSist as bios 
-import histogramas as hist 
 import convergencia_redes as cR
 import math 
-
+import random as rand
 
 
 def distanciaHamming(linha_matriz, linha_possivel):
@@ -29,48 +27,106 @@ def calculos_matriz(mtz):
 
 	return H, W
 
+def dist_Q(h1, w1, h2, w2):
 
-import math
-mtz_WT = dados.matriz_wildtype
-h_WT, w_WT = calculos_matriz(mtz_WT)
+	h = abs(h1 - h2)
+	w = abs(w1 - w2)
+	Q = math.sqrt((h**2) + (w**2))
 
-linhas_possiveis = [0] * 11
 
-for i in range(0, 11):
+	return Q 
+
+### Algoritmo Genetico ###
+
+
+# Cria Populacao #
+
+def varia_gene(mtz, gene):
+
+	var = []
+
+	linhas_gene = bios.abre_arquivo(f'gene{gene}.txt')
+
+	for i in range(0, len(linhas_gene)):
+
+		if distanciaHamming(mtz[i], linhas_gene[j]):
+
+			nova_matriz = mtz.copy()
+			nova_matriz[i] = linhas_gene[j]
+			var.append(nova_matriz)
+
+	return var
+
+def cria_populacao(mtz):
 	
-	linhas_genes = bios.abre_arquivo(f'gene{i}.txt')
-	aux = []
+	pop = []
 
-	for j in range(0, len(linhas_genes)):
+	for i in range(0, 11):
 
-
-		if distanciaHamming(linhas_genes[j], mtz_WT[i]):
-			aux.append(linhas_genes[j])
-
-	linhas_possiveis[i] = aux
+		
+		pop.append(varia_gene(mtz, i))
 
 
-Hs = []
-Ws = []
+	return pop
 
-for i in range(0, 11):
+# Selecao #
 
-	for linha in linhas_possiveis[i]:
+def evento_selecao(mtz, pop, limiar):
 
-		nova_matriz = mtz_WT.copy()
-		nova_matriz[i] = linha
-		h, w = calculos_matriz(nova_matriz)
-		d = math.sqrt((abs(h - h_WT)**2) + (abs((w - w_WT)/2048)**2))
-		if d <= 0.1:
-			Hs.append(h)
-			Ws.append(w/2048)
+	h, w = calculos_matriz(mtz)
+	selecionados = [0] * 11
 
-import matplotlib.pyplot as plt
+	for i in range(0, 11):
+
+		pop_gene = pop[i]
+		aux = []
+		for j in range(0, len(pop_gene)):
+			h_j, w_j = calculos_matriz(pop_gene[j])
+			
+			if limiar >= dist_Q(h, w, h_j, w_j):
+				aux.append(pop_gene[j])
+
+		selecionados[i] = aux
+
+	return selecionados
+		
+# Reproducao #
+
+def recombinacao(mtz_1, mtz_2):
+
+	corte = rand.randint(0, 11)
+	return mtz_1[:corte] + mtz_2[corte:]
 
 
-plt.scatter(Hs, Ws)
-plt.title('Gráfico Entropia(H) por Convergência(W)')
-plt.xlabel('Entropia(H)')
-plt.ylabel('Convergência(W)')
-plt.scatter(h_WT, w_WT/2048, color = 'red')
-plt.show()
+def sorteia_reprodutores(pop):
+
+	gene_1, gene_2 = rand.sample(range(0, 11), 2)
+	tam_1 = len(pop[gene_1])
+	tam_2 = len(pop[gene_2])
+	rep_1 = pop[gene_1][rand.randint(0, tam_1)]
+	rep_2 = pop[gene_2][rand.randint(0, tam_2)]
+
+	return rep_1, rep_2
+
+# Mutacao #
+
+def sorteia_individuos(pop, parcela):
+
+	tam = 0
+	for i in range(0, 11):
+		tam += len(pop[i])
+
+	n_parcela = int(tam*parcela)
+	individuos = []
+
+
+
+
+# Geracoes #
+
+
+def roda_geracoes(n, mtz):
+
+	pop_inicial = cria_populacao(mtz)
+	
+	pass
