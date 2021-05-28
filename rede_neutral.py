d1c89f74cd172ec2f200c25e9dd6a7a716176e05
gpsp95 <45984859+gabriel-piedade95@users.noreply.github.com>
2021-05-28 15:49:46 -0300

    algoritmo de randomwalk para o grafo

--- rede_neutral.py
+++ rede_neutral.py
@@ -4,6 +4,7 @@ import convergencia_redes as cR
 import math 
 import random as rand
 
+### Calculos ###
 
 def distanciaHamming(linha_matriz, linha_possivel):
 
@@ -35,6 +36,102 @@ def dist_Q(h1, w1, h2, w2):
 
 
 	return Q 
+
+
+
+### Stringfica ###
+
+def stringfica_rede(mtz):
+
+	aux = []
+	for i in range(0, len(mtz)):
+
+		linhas_possiveis = bios.abre_arquivo(f'gene{i}.txt')
+
+		for j in range(0, len(linhas_possiveis)):
+
+			if linhas_possiveis[j] == mtz[i]:
+				aux.append(j)
+				break
+
+	return '-'.join(str(x) for x in aux)
+
+def matriz_da_string(string):
+
+	aux = [int(x) for x in string.split('-')]
+	matriz = []
+	for i in range(0, 11):
+		linhas = bios.abre_arquivo(f'gene{i}.txt')
+		linha = linhas[aux[i]]
+		matriz.append(linha)
+
+	return matriz
+
+### Random Walks ###
+
+def randomwalk_grafo(mtz, passos):
+	
+	posicao = stringfica_rede(mtz)
+	caminho = [posicao]
+	rede = mtz.copy()
+
+	i = 0
+
+	while i < passos:
+	
+		gene = rand.randint(0, 10)
+		linhas = bios.abre_arquivo(f'gene{gene}.txt')
+		aux = []
+
+		for j in range(0, len(linhas)):
+
+			if distanciaHamming(rede[gene], linhas[j]):
+				aux.append(linhas[j])
+
+		n = rand.randint(0, len(aux) - 1)
+		nova_rede = rede.copy()
+		nova_rede[gene] = aux[n]
+
+		nova_posicao = stringfica_rede(nova_rede)
+
+		if nova_posicao not in caminho:
+
+			caminho.append(nova_posicao)
+			rede = nova_rede
+			i += 1
+
+	return caminho
+
+
+def imprime_randomwalk(caminho, n):
+
+	with open(f'randomwalk_{n}.txt', 'w', encoding = 'utf-8') as f:
+
+		for i in range(0, len(caminho)):
+
+			for j in range(0, len(caminho[i])):
+
+				f.write(caminho[i][j])
+			f.write('\n')
+
+
+
+mtz = dados.matriz_wildtype
+caminho = randomwalk_grafo(mtz, 1000)
+imprime_randomwalk(caminho, 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
